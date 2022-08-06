8054b06efe0a7f1d52cc33b8e4f9481f04bedac5
David Ajibade <dbaconsults1@gmail.com>
2022-08-06 06:48:53 -0400

    my first commit

---
+++ app.java
@@ -0,0 +1,4 @@
+ Dba Ventures is a Global Service Company
+ Dba helps people to succeed in their most gifted area
+ Dba is set to help 100 million people to become millionaires
+ As they become millionaires, they will get to know that Jesus is Lord
